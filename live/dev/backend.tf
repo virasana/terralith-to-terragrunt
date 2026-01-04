a904437f802317dbef28775e0fb4c0f0959509ca
Jean-Pierre Fouche <jeanpierrefouche@gmail.com>
2026-01-04 18:25:32 +0000

    step 5 complete

--- live/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terragrunt-to-terralith-tfstate-ks-20260104"
-    key          = "dev/tofu.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
